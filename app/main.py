d2257503f07a83e629aad8eb93afdd4fe24b9e25
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-04-22 22:44:25 +0300

    Fixed flake8 #2

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
-def cinema_visit(customers: list, number: int, cleaner: str, movie: str):
+
+def cinema_visit(customers: list, number: int, cleaner: str, movie: str) -> None:
     # write you code here
     cleaner_obj = Cleaner(name=cleaner)
     hall_obj = CinemaHall(number=number)
@@ -11,7 +12,10 @@ def cinema_visit(customers: list, number: int, cleaner: str, movie: str):
     for cus in customers:
         new_customer = Customer(name=cus["name"], food=cus["food"])
         customers_obj.append(new_customer)
-        CinemaBar.sell_product(product=new_customer.food, customer=new_customer)
+        CinemaBar.sell_product(
+            product=new_customer.food,
+            customer=new_customer
+        )
 
     hall_obj.movie_session(
         movie_name=movie,
